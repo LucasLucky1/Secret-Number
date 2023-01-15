daff5ae8d437419d7d50cd62d02cbf2b15be9432
LucasLucky1 <lucaserickson100@gmail.com>
2023-01-15 01:45:22 -0300

    terminando função 'jogar novamente'

--- app/verificaValidacao.js
+++ app/verificaValidacao.js
@@ -12,11 +12,19 @@ function validaFala(fala){
         
     }
     if(numero === numeroSecreto){
-        document.body.innerHTML = `<h3> Você acertou! </h3> <div> O número secreto era ${numeroSecreto}` ;
+        document.body.innerHTML = `<h3> Você acertou! </h3> <div> O número secreto era ${numeroSecreto} 
+        </div><button class= "btn" id='btn'>Jogar novamente </button>` ;
+        //opção de botão reload
+        // const botaoRestart = document.querySelector('.btn');
+        // botaoRestart.addEventListener('click', ()=>{
+        // window.location.reload()
+// })
+       
+        
     }else if(numero > numeroSecreto){
         chute.innerHTML += `<div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>`
 
-    }else if(numero < numeroSecreto){
+    }else{
         chute.innerHTML += `<div> O número secreto é maior <i class="fa-solid fa-up-long"></i></div>`
 
     }
@@ -28,3 +36,10 @@ function valorDentroDoPermitido(numero){
         return true
     }
 }
+
+//botão reload no escopo global
+window.addEventListener('click', e=>{
+    if(e.target.id=='btn'){
+        window.location.reload();
+    }
+})
